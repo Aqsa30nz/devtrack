d6afbae4b7e0701f265932e394d8c3f4c57ff889
Aqsa30nz <aqsa301naaz@gmail.com>
2026-06-19 10:42:35 +0530

    Add service layer and integrate Activity repository updates

---
+++ src/main/java/com/aqsa/devtrack/repository/ActivityRepository.java
@@ -0,0 +1,9 @@
+package com.aqsa.devtrack.repository;
+
+import com.aqsa.devtrack.entity.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+}
