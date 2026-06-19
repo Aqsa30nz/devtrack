ec9cb9920e0ab86d48e60ab6040c8c58c330be56
Aqsa30nz <aqsa301naaz@gmail.com>
2026-06-19 10:52:27 +0530

    Implement ActivityService with CRUD business logic

---
+++ src/main/java/com/aqsa/devtrack/service/ActivityService.java
@@ -0,0 +1,38 @@
+package com.aqsa.devtrack.service;
+
+import com.aqsa.devtrack.entity.Activity;
+import com.aqsa.devtrack.repository.ActivityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    public ActivityService(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
+
+    // CREATE
+    public Activity createActivity(Activity activity) {
+        return activityRepository.save(activity);
+    }
+
+    // READ ALL
+    public List<Activity> getAllActivities() {
+        return activityRepository.findAll();
+    }
+
+    // READ BY ID
+    public Optional<Activity> getActivityById(Long id) {
+        return activityRepository.findById(id);
+    }
+
+    // DELETE
+    public void deleteActivity(Long id) {
+        activityRepository.deleteById(id);
+    }
+}
