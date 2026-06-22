a009f648916a0f1f4af015b5ef039b3a7c49d03f
Aqsa30nz <aqsa301naaz@gmail.com>
2026-06-22 12:08:55 +0530

    feat(auth): add User entity

---
+++ src/main/java/com/aqsa/devtrack/entity/User.java
@@ -0,0 +1,24 @@
+package com.aqsa.devtrack.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+}
