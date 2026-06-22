20c4bea5163d502b546bbd555eba9495f7366100
Aqsa30nz <aqsa301naaz@gmail.com>
2026-06-22 12:10:50 +0530

    feat(auth): add UserRepository

---
+++ src/main/java/com/aqsa/devtrack/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.aqsa.devtrack.repository;
+
+import com.aqsa.devtrack.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
