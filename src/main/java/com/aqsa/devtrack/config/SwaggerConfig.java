e4e1e478657711f8355da0fa6b90681e6bab3b86
Aqsa30nz <aqsa301naaz@gmail.com>
2026-06-26 19:42:44 +0530

    feat: integrate Swagger/OpenAPI with JWT security configuration

---
+++ src/main/java/com/aqsa/devtrack/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package com.aqsa.devtrack.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI devTrackOpenAPI() {
+
+        final String securitySchemeName = "Bearer Authentication";
+
+        return new OpenAPI()
+
+                .info(
+                        new Info()
+                                .title("DevTrack API")
+                                .version("1.0")
+                                .description(
+                                        "REST API for tracking learning activities, analytics, and dashboard insights."
+                                )
+                                .contact(
+                                        new Contact()
+                                                .name("Aqsa Naaz")
+                                                .email("aqsa@example.com")
+                                )
+                )
+
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(securitySchemeName)
+                )
+
+                .schemaRequirement(
+                        securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
+    }
+}
